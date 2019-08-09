87515fcf665650c566051f5a65424383c99b167d
FernandoCendretti <luiz.cendretti@gmail.com>
2019-08-09 19:31:06 -0300

    Validação dos campos de atualização

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 import User from '../models/User';
 
 import authConfig from '../../config/auth';
@@ -10,6 +11,17 @@ class SessionController {
    * @param {Express} res response
    */
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validations fails' });
+    }
+
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
